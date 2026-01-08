05474576cb113d8864970d3eebe97a74f07ffcb6
Adarsh Shukla <adarshshukla09887@gmail.com>
2026-01-08 17:13:08 +0530

    Force fix folder casing for Vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // react port
+    origin: "https://habit-tracker-liart-alpha.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
