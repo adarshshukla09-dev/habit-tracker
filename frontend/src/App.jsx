270b26fa4812afd20de6e00471696c8752bcc0c1
Adarsh Shukla <adarshshukla09887@gmail.com>
2026-01-08 16:28:50 +0530

    Force fix folder casing for Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./Context/Dashboard.jsx";
+import Dashboard from "./Pages/Dashboard.jsx";
 import Login from "./Pages/Login.jsx";
 import Signup from "./Pages/Signup.jsx";
 import { AuthProvider } from "./Context/UserContext.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
-import HomePage from "./Pages/HomePage.jsx";
+import HomePage from "./Pages/HomePage.js";
 
 function App() {
   return (
